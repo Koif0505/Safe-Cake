748e5bf4452cc783112bdafab86b37eb3de583e5
Koif0505 <truongbinhnhi2021py@gmail.com>
2026-03-30 09:51:43 +0700

    đổi VR game

---
+++ Assets/Scipt/CameraFollowVR.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraFollowVR : MonoBehaviour
+{
+    public Transform neckBone; // Kéo xương Neck vào đây
+    public Vector3 offset = new Vector3(0, 0, 0.15f); // Đẩy ra trước một chút để không thấy "ruột" nhân vật
+
+    void LateUpdate() // Dùng LateUpdate để chạy sau khi hoạt ảnh đã xong, giúp hết giật
+    {
+        if (neckBone != null)
+        {
+            // CHỈ LẤY VỊ TRÍ: Camera sẽ di chuyển theo cổ
+            transform.position = neckBone.position + neckBone.TransformDirection(offset);
+
+            // KHÔNG LẤY XOAY: Xoay sẽ do Tracked Pose Driver (Kính VR) 
+            // hoặc Script xoay của cậu tự lo. 
+        }
+    }
+}
