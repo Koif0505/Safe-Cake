481adceba70aded2f42e3fb68c3bafce2a6c0784
Koif0505 <truongbinhnhi2021py@gmail.com>
2026-04-03 10:00:52 +0700

    .

--- Assets/Scipt/NewBehaviourScript.cs
+++ Assets/Scipt/NewBehaviourScript.cs
@@ -1,10 +1,21 @@
 using UnityEngine;
 
-public class InputDebugAll : MonoBehaviour
+public class InputDebugButtons : MonoBehaviour
 {
     void Update()
     {
-        // Gamepad buttons
+        if (Input.GetMouseButtonDown(0)) Debug.Log("Mouse Left");
+        if (Input.GetMouseButtonDown(1)) Debug.Log("Mouse Right");
+        if (Input.GetMouseButtonDown(2)) Debug.Log("Mouse Middle");
+
+        if (Input.GetKeyDown(KeyCode.Return)) Debug.Log("Enter");
+        if (Input.GetKeyDown(KeyCode.Space)) Debug.Log("Space");
+        if (Input.GetKeyDown(KeyCode.Escape)) Debug.Log("Escape");
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) Debug.Log("LeftArrow");
+        if (Input.GetKeyDown(KeyCode.RightArrow)) Debug.Log("RightArrow");
+        if (Input.GetKeyDown(KeyCode.UpArrow)) Debug.Log("UpArrow");
+        if (Input.GetKeyDown(KeyCode.DownArrow)) Debug.Log("DownArrow");
+
         for (int i = 0; i <= 19; i++)
         {
             if (Input.GetKeyDown((KeyCode)((int)KeyCode.JoystickButton0 + i)))
@@ -12,27 +23,5 @@ public class InputDebugAll : MonoBehaviour
                 Debug.Log("Joystick button: " + i);
             }
         }
-
-        // Mouse
-        if (Input.GetMouseButtonDown(0)) Debug.Log("Mouse Left");
-        if (Input.GetMouseButtonDown(1)) Debug.Log("Mouse Right");
-        if (Input.GetMouseButtonDown(2)) Debug.Log("Mouse Middle");
-
-        // Some common keyboard-like keys
-        if (Input.anyKeyDown)
-        {
-            Debug.Log("Any key down detected");
-        }
-
-        // Axes
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
-        float mx = Input.GetAxisRaw("Mouse X");
-        float my = Input.GetAxisRaw("Mouse Y");
-
-        if (Mathf.Abs(h) > 0.1f) Debug.Log("Horizontal: " + h);
-        if (Mathf.Abs(v) > 0.1f) Debug.Log("Vertical: " + v);
-        if (Mathf.Abs(mx) > 0.1f) Debug.Log("Mouse X: " + mx);
-        if (Mathf.Abs(my) > 0.1f) Debug.Log("Mouse Y: " + my);
     }
 }
